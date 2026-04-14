ab8d196b0710cddb48c373ebbec6e5979951b022
John McLear <john@mclear.co.uk>
2026-04-14 18:06:33 +0100

    feat: scaffold ep_ai_core plugin with loadSettings hook

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+'use strict';
+
+require('eslint-config-etherpad/patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: 'etherpad/plugin',
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const log4js = require('ep_etherpad-lite/node_modules/log4js');
+const logger = log4js.getLogger('ep_ai_core');
+
+let pluginSettings = null;
+
+const getSettings = () => pluginSettings;
+
+exports.loadSettings = async (hookName, {settings}) => {
+  pluginSettings = settings.ai || {};
+  if (!pluginSettings.access) {
+    pluginSettings.access = {defaultMode: 'full', pads: {}};
+  }
+  if (!pluginSettings.access.defaultMode) {
+    pluginSettings.access.defaultMode = 'full';
+  }
+  logger.info('ep_ai_core settings loaded');
+  logger.info(`Default access mode: ${pluginSettings.access.defaultMode}`);
+};
+
+exports.init_ep_ai_core = async (hookName, {logger: l}) => {
+  logger.info('ep_ai_core initialized');
+};
+
+exports.getSettings = getSettings;
